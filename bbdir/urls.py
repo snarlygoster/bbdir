fc1aa2b526547b188e7e2aef07383c9d66dc2a6d
David Eves <deves@pobox.com>
2011-08-30 19:49:03 -0700

    urls and views using chunkpaginator

--- bbdir/urls.py
+++ bbdir/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
-from django.views.generic import DetailView, ListView, UpdateView, CreateView
+#from django.views.generic import DetailView, ListView, UpdateView, CreateView
 
-from bbdir.models import Entry
+from bbdir.views import BinderDirectoryListView, BinderDirectoryDetailView
+
 
 urlpatterns = patterns('', 
-    (r'^$', ListView.as_view(model = Entry)),
-    url(r'^(?P<slug>[\w-]+)/$', DetailView.as_view(model=Entry,), name = 'entry-detail' ),
-    url(r'^(?P<slug>[\w-]+)/edit/$', UpdateView.as_view(model=Entry,), name = 'entry-edit' ),
+    url(r'^(?P<sort_by>[\w]+)/$', BinderDirectoryListView.as_view(), name='entry-list'),
+    url(r'^(?P<sort_by>[\w]+)/(?P<slug>[\w-]+)/$', BinderDirectoryDetailView.as_view(), name = 'entry-detail' ),
    
 )
--- bbdir/views.py
+++ bbdir/views.py
@@ -1,1 +1,62 @@
 # Create your views here.
+from django.views.generic import DetailView, ListView
+
+from bbdir.models import Entry
+from namepaginator.chunkpaginator import ChunkPaginator
+
+def create_namepaginator(request, object_list, on='name', per_page = 10):
+    
+    paginator = NamePaginator(object_list, on = on, per_page = per_page)
+    
+    print on
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        page = paginator.page(page)
+    except (InvalidPage):
+        page = paginator.page(paginator.num_pages)
+    return page
+
+
+class BinderDirectoryListView(ListView):
+    """
+class PublisherDetailView(DetailView):
+
+    context_object_name = "publisher"
+    model = Publisher
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PublisherDetailView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['book_list'] = Book.objects.all()
+        return context
+        
+    """
+    model = Entry
+    per_page = 10
+    paginator_class = ChunkPaginator
+    paginate_by = 20
+    
+    def get_queryset(self):
+        self.on = self.kwargs['sort_by']
+        
+        
+        return Entry.objects.order_by(self.on)
+        
+        
+    def get_paginator(self, queryset, per_page, orphans=0, allow_empty_first_page=True):
+        print self.model
+        return ChunkPaginator(queryset, per_page, orphans, allow_empty_first_page, on=self.on)
+
+#     def get_context_data(self, **kwargs):
+#         context = super(BinderDirectoryListView, self).get_context_data(**kwargs)
+#         print 'Here'
+#         context['page'] = create_namepaginator(self.request, self.object_list, on='name', per_page = self.per_page)
+#         return context
+        
+
+class BinderDirectoryDetailView(DetailView):
+    model=Entry
